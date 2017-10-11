e6921062a5fdc6e0cf2518aba6d6fe08fd398a69
Irenee Groz <irenee.groz@ensimag.grenoble-inp.fr>
2017-10-11 12:11:22 +0200

    update client

--- Client/src/client/DaytimeClient.java
+++ Client/src/client/DaytimeClient.java
@@ -1,4 +1,4 @@
-package CLIENT;
+package client;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,26 +7,40 @@ import java.net.UnknownHostException;
 
 public class DaytimeClient {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        if (args.length != 4) {
+            System.out.println("Erreur, nombre d'arguments incorrect !");
+            System.exit(0);
+        }
+        String hostname = args[0];
+        String port = args[1];
+        String request = args[2];
+        String filename = args[3];
+        try {
+            Socket theSocket = new Socket(hostname, Integer.parseInt(port));
+            InputStream timeStream = theSocket.getInputStream();
+            StringBuffer time = new StringBuffer();
+            //String hostname = "localhost";
+            switch (request) {
+                case "GET_REQUEST":
+                    int c;
+                    while ((c = timeStream.read()) != -1) {
+                        time.append((char) c);
+                    }
+                    String timeString = time.toString().trim();
+                    System.out.println("It is " + timeString + " at " + hostname);
 
-    String hostname = "localhost";
+                case "PUT_REQUEST": {
+                }
+                ;
+            }
+        } catch (UnknownHostException ex) {
+            System.err.println(ex);
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
 
-    try {
-      Socket theSocket = new Socket(hostname, 2000);
-      InputStream timeStream = theSocket.getInputStream();
-      StringBuffer time = new StringBuffer();
-      int c;
-      while ((c = timeStream.read()) != -1)
-        time.append((char) c);
-      String timeString = time.toString().trim();
-      System.out.println("It is " + timeString + " at " + hostname);
-    } // end try
-    catch (UnknownHostException ex) {
-      System.err.println(ex);
-    } catch (IOException ex) {
-      System.err.println(ex);
     }
 
-  }
-
 }
+
---
+++ Client/src/client/FTPFile.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import java.nio.file.Path;
+import java.nio.file.*;
+import java.io.*;
+/**
+ *
+ * @author grozir
+ */
+public class FTPFile {
+  String filename = "";
+  File data;
+  public FTPFile(String name){
+      this.filename = name;
+      if (this.exists()) this.getData();
+  }
+  public boolean exists()
+  {
+      java.io.File f = new java.io.File("./" + filename);
+      boolean  a = f.exists();
+
+      if(a)
+      {
+          System.out.println("ça va, ca existe");
+      }
+      else
+      {
+          System.out.println("NOOOOOOOOOOOOOOOOOOOOON");
+      }
+      return a;
+  }
+  public void getData()
+  {
+      if (this.exists())
+      {
+          this.data= new File(filename);
+          System.out.println("Le fichier est bien chargé");
+      }
+      else
+      {
+          System.out.println("Yapadfichier");
+      }
+  }
+  public void writeFile()
+  {
+      FileInputStream instream = null;
+      FileOutputStream outstream = null;
+      try 
+      {
+          Path currentRelativePath = Paths.get("");
+          String s = currentRelativePath.toAbsolutePath().toString();
+          
+          File infile = this.data;
+          File outfile = new File(s+"/"+filename);
+          
+          instream = new FileInputStream(infile);
+          outstream = new FileOutputStream(outfile);
+          
+          byte[] buffer = new byte[1024];
+          
+          int length;
+          
+          while((length = instream.read(buffer)) > 0)
+          {
+              outstream.write(buffer,0,length);
+          }
+          instream.close();
+          outstream.close();
+          
+          System.out.println("l'écriture du fichier a marché =)");
+          
+      }
+      catch(IOException ioe)
+      {
+          ioe.printStackTrace();
+      }
+  }
+}
