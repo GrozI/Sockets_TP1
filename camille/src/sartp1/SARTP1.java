13097dc4a4061db173c55015b41f577fd30781e8
Camille Gardelle <camille.gardelle@phelma.grenoble-inp.fr>
2017-10-18 10:02:29 +0200

    ajout de ma partie (non finie)
    contenue dans le dossier camille

---
+++ camille/src/sartp1/SARTP1.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sartp1;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.io.File;
+import java.io.ByteArrayInputStream;
+
+/**
+ *
+ * @author gardellc
+ */
+public class SARTP1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String hostname = "localhost";
+        int port = 2000;
+        String repertory = System.getProperty("user.dir")+"/clientFiles";
+        
+        try {
+          Socket theSocket = new Socket(hostname, port );
+          System.out.println("connécté à "+hostname+" sur le port "+port);
+          
+          InputStream timeStream = theSocket.getInputStream();
+          
+          
+          /*int b;
+          byte[] table = new byte[1];
+          byte[] table2 = new byte[1];
+          timeStream.read(table);
+          timeStream.read(table2);
+          //while ((b = timeStream.read()) != -1){
+              //System.out.println("aaa");
+          //}
+          System.out.println(table[0]);
+          System.out.println(table2[0]);
+          */
+          
+          //InputStream timeStream = theSocket.getInputStream();
+          //StringBuffer time = new StringBuffer();
+          //int c;
+          //while ((c = timeStream.read()) != -1)
+            //time.append((char) c);
+          //String timeString = time.toString().trim();
+          //System.out.println("It is " + timeString + " at " + hostname);
+          //System.out.println(timeString);
+          
+          
+          
+        } // end try
+        catch (UnknownHostException ex) {
+          System.err.println(ex);
+        } catch (IOException ex) {
+          System.err.println(ex);
+        }
+        
+        
+    }
+    
+}
---
+++ camille/src/sartp1/SimpleDaytimeServer.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sartp1;
+
+/*
+ * Copyright (c) 2004 David Flanagan.  All rights reserved.
+ * This code is from the book Java Examples in a Nutshell, 3nd Edition.
+ * It is provided AS-IS, WITHOUT ANY WARRANTY either expressed or implied.
+ * You may study, use, and modify it for any non-commercial purpose,
+ * including teaching and use in open-source projects.
+ * You may distribute it non-commercially as long as you retain this notice.
+ * For a commercial use license, or to purchase the book, 
+ * please visit http://www.davidflanagan.com/javaexamples3.
+ */
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import java.io.File;
+
+
+/**
+ * A simple TCP server for the daytime service. See RFC867 for details. This
+ * implementation lacks meaningful exception handling and cannot handle UDP
+ * connections.
+ */
+public class SimpleDaytimeServer {
+  public static void main(String args[]) throws java.io.IOException {
+    // RFC867 specifies port 13 for this service. On Unix platforms,
+    // you need to be running as root to use that port, so we allow
+    // this service to use other ports for testing.
+    int port = 2000; //au dessus de 1024 sur linux
+    if (args.length > 0)
+      port = Integer.parseInt(args[0]);
+    
+    String repertory = System.getProperty("user.dir")+"/serverFiles";
+    System.out.println(repertory);
+    
+    // Create a channel to listen for connections on.
+    ServerSocketChannel server = ServerSocketChannel.open();
+    
+    // Bind the channel to a local port. Note that we do this by obtaining
+    // the underlying java.net.ServerSocket and binding that socket.
+    server.socket().bind(new InetSocketAddress(port));
+    System.out.println(server.toString());
+    
+    // Get an encoder for converting strings to bytes
+    CharsetEncoder encoder = Charset.forName("US-ASCII").newEncoder();
+
+    for (;;) { // Loop forever, processing client connections
+      // Wait for a client to connect
+      SocketChannel client = server.accept();
+
+      // Build response string, wrap, and encode to bytes
+      
+      //String date = new java.util.Date().toString() + "\r\n";
+      //ByteBuffer response = encoder.encode(CharBuffer.wrap(date));
+      //System.out.println("response"+response.toString());
+      
+      //FTPcmd message = new FTPcmd("name","content");
+      //ByteBuffer response = ByteBuffer.wrap(BytesUtil.toByteArray(message));
+      //byte[] byteArray = {56,58};
+      //System.out.println(byteArray[0] +" "+byteArray[1]);
+      //ByteBuffer response = ByteBuffer.wrap(byteArray);
+      
+      File data = new File("test.odt");
+      byte[] byteArray = BytesUtil.toByteArray(data);
+      ByteBuffer response = ByteBuffer.wrap(byteArray);
+      
+      // Send the response to the client and disconnect.
+      client.write(response);
+      client.close();
+    }
+  }
+}
